4b5eb61af4b498f3432dabc6bbc971ed8aebe562
Eorihs <pikapau.comim@gmail.com>
2022-03-26 07:03:00 -0300

    Third commit
    {EN-US}
    Desktop login screen completed, this commit is for testing on some mobile devices that I have physically available.
    
    {PT-BR}
    Tela de login para desktop concluída, essa commit é para testar em alguns dispositivos mobile que tenho disponível fisicamente.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
-import Link from "next/link"
 import React from "react"
+import { useRouter } from "next/router"
 
 const Home = function () {
+  const router = useRouter();
   return (
     <div className="Home__container">
+      <div></div>
       <div className="Home__container__screen">
         <h2>Bem vindo!</h2>
-        <p>Para acessar a plataforma, continue abaixo:</p>
-        <Link href='/login' passHref>
-          <button>Entrar</button>
-        </Link>
+        <p>Para acessar a plataforma, continue <span>abaixo:</span></p>
+        <button onClick={() => router.push("/login")}>Entrar</button>
       </div>
     </div>
   )
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,28 +1,64 @@
-import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 import { IoPersonCircleOutline } from 'react-icons/io5';
 import { RiLockPasswordLine } from 'react-icons/ri';
 
 const Login = function () {
+   const [button, setButton] = useState(false);
+   const router = useRouter();
+
+   function handleSubmit(e: React.SyntheticEvent) {
+      e.preventDefault();
+      const target = e.target as typeof e.target & {
+         login: { value: string };
+         password: { value: string };
+      };
+      
+      const login = target.login.value;
+      const password = target.password.value;
+
+      if (login != "" && password != "") {
+         router.push("/home");
+      } else {
+         alert("Complete todos os campos");
+      }
+   }
    return (
-      <div className='Login__container'>
+      <form className='Login__container' onSubmit={handleSubmit}>
+         <div></div>
          <div className='Login__container__screen'>
             <h2>Bem vindo!</h2>
             <div>
                <IoPersonCircleOutline />
-               <input type='text' placeholder='teste@none.com' />
+               <input
+                  name='login'
+                  type='text'
+                  placeholder="Nome de usuário"
+                  onChange={
+                     (e) => e.target.value.length > 0 ? setButton(true) : setButton(false)}
+                  />
             </div>
 
             <div>
                <RiLockPasswordLine />
-               <input type='password' placeholder='•••••••' />
+               <input
+                  name='password'
+                  type='password'
+                  placeholder="Senha"
+                  onChange={
+                     (e) => e.target.value.length > 0 ? setButton(true) : setButton(false)}
+                  />
             </div>
 
-            <Link href='/home' passHref>
-               <button>Logar</button>
-            </Link>
+            <button
+               type="submit"
+               className={`${button ? "-active" : ""}`}
+            >
+               Logar
+            </button>
          </div>
-      </div>
+      </form>
    )
 }
 
